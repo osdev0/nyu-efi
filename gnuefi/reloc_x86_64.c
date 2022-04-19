8cb133d804d8e80900b45e6449364118820e0bd9
mintsuki <mintsuki@protonmail.com>
2022-04-19 23:41:33 +0200

    x86_64: Misc bug fixes and improvements

--- gnuefi/reloc_x86_64.c
+++ gnuefi/reloc_x86_64.c
@@ -1,8 +1,9 @@
 /* reloc_x86_64.c - position independent x86_64 ELF shared object relocator
    Copyright (C) 1999 Hewlett-Packard Co.
-	Contributed by David Mosberger <davidm@hpl.hp.com>.
+    Contributed by David Mosberger <davidm@hpl.hp.com>.
    Copyright (C) 2005 Intel Co.
-	Contributed by Fenghua Yu <fenghua.yu@intel.com>.
+    Contributed by Fenghua Yu <fenghua.yu@intel.com>.
+   Copyright (C) 2022 Mintsuki
 
     All rights reserved.
 
@@ -40,57 +41,57 @@
 
 #include <elf.h>
 
-EFI_STATUS _relocate (long ldbase, Elf64_Dyn *dyn)
+EFI_STATUS __relocate (long ldbase, Elf64_Dyn *dyn)
 {
-	long relsz = 0, relent = 0;
-	Elf64_Rel *rel = 0;
-	unsigned long *addr;
-	int i;
+    long relsz = 0, relent = 0;
+    Elf64_Rela *rel = 0;
+    unsigned long *addr;
+    int i;
 
-	for (i = 0; dyn[i].d_tag != DT_NULL; ++i) {
-		switch (dyn[i].d_tag) {
-			case DT_RELA:
-				rel = (Elf64_Rel*)
-					((unsigned long)dyn[i].d_un.d_ptr
-					 + ldbase);
-				break;
+    for (i = 0; dyn[i].d_tag != DT_NULL; ++i) {
+        switch (dyn[i].d_tag) {
+            case DT_RELA:
+                rel = (Elf64_Rela*)
+                    ((unsigned long)dyn[i].d_un.d_ptr
+                     + ldbase);
+                break;
 
-			case DT_RELASZ:
-				relsz = dyn[i].d_un.d_val;
-				break;
+            case DT_RELASZ:
+                relsz = dyn[i].d_un.d_val;
+                break;
 
-			case DT_RELAENT:
-				relent = dyn[i].d_un.d_val;
-				break;
+            case DT_RELAENT:
+                relent = dyn[i].d_un.d_val;
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-        if (!rel && relent == 0)
-                return EFI_SUCCESS;
+    if (!rel && relent == 0)
+        return EFI_SUCCESS;
 
-	if (!rel || relent == 0)
-		return EFI_LOAD_ERROR;
+    if (!rel || relent == 0)
+        return EFI_LOAD_ERROR;
 
-	while (relsz > 0) {
-		/* apply the relocs */
-		switch (ELF64_R_TYPE (rel->r_info)) {
-			case R_X86_64_NONE:
-				break;
+    while (relsz > 0) {
+        /* apply the relocs */
+        switch (ELF64_R_TYPE (rel->r_info)) {
+            case R_X86_64_NONE:
+                break;
 
-			case R_X86_64_RELATIVE:
-				addr = (unsigned long *)
-					(ldbase + rel->r_offset);
-				*addr += ldbase;
-				break;
+            case R_X86_64_RELATIVE:
+                addr = (unsigned long *)
+                    (ldbase + rel->r_offset);
+                *addr = ldbase + rel->r_addend;
+                break;
 
-			default:
-				break;
-		}
-		rel = (Elf64_Rel*) ((char *) rel + relent);
-		relsz -= relent;
-	}
-	return EFI_SUCCESS;
+            default:
+                return EFI_LOAD_ERROR;
+        }
+        rel = (Elf64_Rela*) ((char *) rel + relent);
+        relsz -= relent;
+    }
+    return EFI_SUCCESS;
 }
