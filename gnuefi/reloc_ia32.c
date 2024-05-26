4bb3d57a0a5861a27eda66192c3ebc32632f8da3
mintsuki <mintsuki@protonmail.com>
2024-05-26 07:11:49 +0200

    Reformat reloc_ia32.c

--- gnuefi/reloc_ia32.c
+++ gnuefi/reloc_ia32.c
@@ -1,6 +1,6 @@
 /* reloc_ia32.c - position independent x86 ELF shared object relocator
    Copyright (C) 1999 Hewlett-Packard Co.
-	Contributed by David Mosberger <davidm@hpl.hp.com>.
+   Contributed by David Mosberger <davidm@hpl.hp.com>.
    Copyright (C) 2022-2024 mintsuki
 
     All rights reserved.
@@ -40,62 +40,62 @@
 
 EFI_STATUS __relocate (long ldbase, Elf32_Dyn *dyn)
 {
-	long relsz = 0, relent = 0;
-	Elf32_Rel *rel = 0;
-	unsigned long *addr;
-	int i;
+    long relsz = 0, relent = 0;
+    Elf32_Rel *rel = 0;
+    unsigned long *addr;
+    int i;
 
-	for (i = 0; dyn[i].d_tag != DT_NULL; ++i) {
-		switch (dyn[i].d_tag) {
-			case DT_REL:
-				rel = (Elf32_Rel*)
-					((unsigned long)dyn[i].d_un.d_ptr
-					 + ldbase);
-				break;
+    for (i = 0; dyn[i].d_tag != DT_NULL; ++i) {
+        switch (dyn[i].d_tag) {
+            case DT_REL:
+                rel = (Elf32_Rel*)
+                    ((unsigned long)dyn[i].d_un.d_ptr
+                     + ldbase);
+                break;
 
-			case DT_RELSZ:
-				relsz = dyn[i].d_un.d_val;
-				break;
+            case DT_RELSZ:
+                relsz = dyn[i].d_un.d_val;
+                break;
 
-			case DT_RELENT:
-				relent = dyn[i].d_un.d_val;
-				break;
+            case DT_RELENT:
+                relent = dyn[i].d_un.d_val;
+                break;
 
-			case DT_RELA:
-				break;
+            case DT_RELA:
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-        if (!rel && relent == 0)
-                return EFI_SUCCESS;
+    if (!rel && relent == 0)
+        return EFI_SUCCESS;
 
-	if (!rel || relent == 0)
-		return EFI_LOAD_ERROR;
+    if (!rel || relent == 0)
+        return EFI_LOAD_ERROR;
 
-	while (relsz > 0) {
-		/* apply the relocs */
-		switch (ELF32_R_TYPE (rel->r_info)) {
-			case R_386_NONE:
-				break;
+    while (relsz > 0) {
+        /* apply the relocs */
+        switch (ELF32_R_TYPE (rel->r_info)) {
+            case R_386_NONE:
+                break;
 
-			case R_386_RELATIVE:
+            case R_386_RELATIVE:
                 if (rel->r_offset < 0x1000) {
                     return EFI_LOAD_ERROR;
                 }
 
-				addr = (unsigned long *)
-					(ldbase + rel->r_offset);
-				*addr += ldbase;
-				break;
+                addr = (unsigned long *)
+                    (ldbase + rel->r_offset);
+                *addr += ldbase;
+                break;
 
-			default:
-				break;
-		}
-		rel = (Elf32_Rel*) ((char *) rel + relent);
-		relsz -= relent;
-	}
-	return EFI_SUCCESS;
+            default:
+                return EFI_LOAD_ERROR;
+        }
+        rel = (Elf32_Rel*) ((char *) rel + relent);
+        relsz -= relent;
+    }
+    return EFI_SUCCESS;
 }
